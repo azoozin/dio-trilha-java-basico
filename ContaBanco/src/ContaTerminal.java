d477f2d617439d11b53c1d0244f65a277ed33cd6
azoozin <mario.github@protonmail.com>
2024-05-06 11:07:34 -0300

    feat: receber input usuario, print output dados
    scan de input do usuario, processar os dados recebidos e devolver output no terminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+        String nomeCliente;
+        String agencia;
+        int numeroConta;
+        double saldo;
+
+        while (true) {
+            System.out.println("Por favor, digite o nome do cliente.");
+            nomeCliente = scan.nextLine();
+            System.out.println("Por favor digite o numero da agencia.");
+            agencia = scan.nextLine();
+            System.out.println("Por favor digite o numero da conta.");
+            numeroConta = Integer.parseInt(scan.nextLine());
+            System.out.println("Por favor digite o saldo.");
+            saldo = Double.parseDouble(scan.nextLine());
+
+            System.out.println("Gostaria de inserir dados diferentes? Digite Sim ou Nao.");
+            if (scan.nextLine().equalsIgnoreCase("Sim")) {
+                continue;
+            }
+            break;
+        }
+
+        System.out.println("Ola " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agencia e "
+                + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " ja esta disponivel para saque.");
+        scan.close();
     }
 }
